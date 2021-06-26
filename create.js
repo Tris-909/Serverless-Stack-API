72331160af59345bc9570c9f615692d14d005756
Tri Tran <tranminhtri9090@gmail.com>
2021-06-26 16:52:57 +1000

    (refactor) Handler for labmda functions and dynamoDB

--- create.js
+++ create.js
@@ -1,33 +1,21 @@
+import handler from './libs/handler-libs';
+import dynamoDB from './libs/dynamodb-libs';
 import * as uuid from 'uuid';
-import AWS from 'aws-sdk';
 
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
-
-export async function main(event, context) {
+export const main = handler(async (event, context) => {
     const data = JSON.parse(event.body);
-
     const params = {
-        TableName: process.env.tableName,
+        TableName: process.env.TableName,
         Item: {
             userId: "123",
             noteId: uuid.v1(),
             content: data.content,
             attachment: data.attachment,
-            createdAt: Date.now(),
+            createdAt: Date.now(), 
         },
     };
 
-    try {
-        await dynamoDB.put(params).promise();
+    await dynamoDB.put(params);
 
-        return {
-            statusCode: 200,
-            body: JSON.stringify(params.Item)
-        };
-    } catch(err) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: err.message })
-        };
-    }
-}
+    return params.Item;
+});
