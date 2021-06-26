ebfe177d500139ed627b51a1c8d2b6316d4114fc
Tri Tran <tranminhtri9090@gmail.com>
2021-06-26 19:28:25 +1000

    (feat) Deploying APIs and fixing serverless.yaml file

--- create.js
+++ create.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import handler from './libs/handler-libs';
 import dynamoDB from './libs/dynamodb-libs';
 import * as uuid from 'uuid';
@@ -14,7 +15,6 @@ export const main = handler(async (event, context) => {
             createdAt: Date.now(), 
         },
     };
-
     await dynamoDB.put(params);
 
     return params.Item;
