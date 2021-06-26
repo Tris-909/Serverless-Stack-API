a532eefeca2d0ed2837202a62f6783cb79b28484
Tri Tran <tranminhtri9090@gmail.com>
2021-06-26 18:58:24 +1000

    (feat) Adding update notes API

---
+++ update.js
@@ -0,0 +1,23 @@
+import handler from './libs/handler-libs';
+import dynamoDB from './libs/dynamodb-libs';
+
+export const main = handler(async (event, context) => {
+    const data = JSON.parse(event.body);
+    const params = {
+        TableName: process.env.TableName,
+        Key: {
+            userId: "123",
+            noteId: event.pathParameters.id,
+        },
+        UpdateExpression: "SET content = :content, attachment = :attachment",
+        ExpressionAttributeValues: {
+            ":attachment": data.attachment || null,
+            ":content": data.content || null,
+        },
+        ReturnValues: "ALL_NEW",
+    };
+
+    await dynamoDB.update(params);
+
+    return { status: true };
+});
