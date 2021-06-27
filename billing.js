60a895bd11cc72504f9a0031abc9b7ff9b31874f
Tri Tran <tranminhtri9090@gmail.com>
2021-06-27 11:16:39 +1000

    (feat) Adding billing API

---
+++ billing.js
@@ -0,0 +1,20 @@
+import stripePackage  from 'stripe';
+import handler from './libs/handler-libs';
+import { calculateCost } from './libs/billing-libs';
+
+export const main = handler(async (event, context) => {
+    const { storage, source } = JSON.parse(event.body);
+    const amount = calculateCost(storage);
+    const description = "Scratch charge";
+
+    const stripe = stripePackage(process.env.stripeSecretKey);
+
+    await stripe.charges.create({
+        source,
+        amount,
+        description,
+        currency: 'usd',
+    });
+
+    return { status: true };
+});
